904bf3df09ff2fba78f9ae544f605c789614f046
antonharkevich <61267091+antonharkevich@users.noreply.github.com>
2020-03-21 17:37:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //fr-FR en-Us en-ES de-De ru-Ru
+            string str = Console.ReadLine();
+            CultureInfo culture = CultureInfo.CreateSpecificCulture(str);//переменная спецификатора культуры
+            DateTime someDate = new DateTime();//начальная дата 01.01.
+            for (int i = 0; i < 12; i++)
+            {
+               
+                Console.WriteLine(someDate.AddMonths(i).ToString("MMMM", culture));//в реальности кроме строки нигде не добавляется// формат MMMM вывод полного значения месяца 
+            }
+        }
+    }
+}
